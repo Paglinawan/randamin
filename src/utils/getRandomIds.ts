35e08ee2b24e8315ae32ec32d54478aff6766b71
miho <paglinawan0520@gmail.com>
2025-01-02 08:40:39 +0900

    Rename: frequency -> rate

--- src/utils/getRandomIds.ts
+++ src/utils/getRandomIds.ts
@@ -1,20 +1,20 @@
 type DataType = { id: number; [key: string]: string | number | boolean }
 
 const getRandomIds = (data: DataType[], count = 3) => {
-  const totalFrequency = data.reduce(
-    (total, item) => total + (Number(item.frequency) || 0),
+  const totalRate = data.reduce(
+    (total, item) => total + (Number(item.rate) || 0),
     0,
   )
 
   const selectedIds: number[] = []
 
   for (let i = 0; i < count; i++) {
-    let randomValue = Math.random() * totalFrequency
-    let cumulativeFrequency = 0
+    let randomValue = Math.random() * totalRate
+    let cumulativeRate = 0
 
     for (const item of data) {
-      cumulativeFrequency += Number(item.frequency) || 0
-      if (randomValue < cumulativeFrequency) {
+      cumulativeRate += Number(item.rate) || 0
+      if (randomValue < cumulativeRate) {
         selectedIds.push(item.id)
         break
       }
